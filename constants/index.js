c98d1be48c993657e89f91251e53af485ef63e1c
zym2525 <540361168@qq.com>
2021-03-26 11:15:22 +0800

    change

--- constants/index.js
+++ constants/index.js
@@ -5,10 +5,13 @@ const TemplateNameType = {
 
 const TemplateRepoUrl = 'git+https://github.com/zym2525/template.git';
 
+const TemplatePackageName = '@zero-d/zerod-template'
+
 const TemplateRepoInfoUrl = 'https://api.github.com/repos/zym2525/template/releases/latest';
 
 module.exports = {
     TemplateNameType,
     TemplateRepoUrl,
-    TemplateRepoInfoUrl
+    TemplateRepoInfoUrl,
+    TemplatePackageName
 }
